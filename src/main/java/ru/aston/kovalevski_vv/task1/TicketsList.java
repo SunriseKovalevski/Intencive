722b3cfb8a93469da47b358ac303376052eae0a0
Vladislav <vladislav.kovalevski.90@gmail.com>
2025-02-06 14:50:49 +0300

    Added class TicketsList with realisation

---
+++ src/main/java/ru/aston/kovalevski_vv/task1/TicketsList.java
@@ -0,0 +1,23 @@
+package ru.aston.kovalevski_vv.task1;
+
+import ru.aston.kovalevski_vv.task1.model.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketsList {
+
+    private ArrayList<Ticket> ticketList = new ArrayList<>();
+
+    public ArrayList<Ticket> getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(ArrayList<Ticket> ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    public void printTicketList() {
+        ticketList.forEach(System.out::println);
+    }
+}
