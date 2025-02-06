f2abca0ca0c06b0c080ab29b523ffecb10bcfa0b
Vladislav <vladislav.kovalevski.90@gmail.com>
2025-02-06 14:41:51 +0300

    Added realisation for international tickets and discount

--- src/main/java/ru/aston/kovalevski_vv/task1/model/International.java
+++ src/main/java/ru/aston/kovalevski_vv/task1/model/International.java
@@ -1,4 +1,15 @@
 package ru.aston.kovalevski_vv.task1.model;
 
-public class International {
+public class International extends  Ticket{
+    public International(Integer id, User user, int cost) {
+        super(id, user, cost);
+        setDiscount();
+    }
+
+    @Override
+    public void setDiscount() {
+        if (getCost() >= 5000) {
+            setCost((int) (getCost() * 0.95));
+        }
+    }
 }
