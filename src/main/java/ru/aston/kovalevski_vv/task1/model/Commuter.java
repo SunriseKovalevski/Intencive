e9f9d9296c8c40e61398399a192e5f93296980df
Vladislav <vladislav.kovalevski.90@gmail.com>
2025-02-06 14:41:35 +0300

    Added realisation for commuter tickets and discount

--- src/main/java/ru/aston/kovalevski_vv/task1/model/Commuter.java
+++ src/main/java/ru/aston/kovalevski_vv/task1/model/Commuter.java
@@ -1,4 +1,17 @@
 package ru.aston.kovalevski_vv.task1.model;
 
-public class Commuter {
+public class Commuter extends Ticket{
+
+    public Commuter(Integer id, User user, Integer cost, Integer discount) {
+        super(id, user, cost);
+        setDiscount();
+
+    }
+
+    @Override
+    public void setDiscount() {
+        if (getCost() >= 1000) {
+            setCost((int) (getCost() * 0.9));
+        }
+    }
 }
