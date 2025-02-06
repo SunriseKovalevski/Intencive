6a101b7171b2b118ee3c667482b3cd2be35478aa
Vladislav <vladislav.kovalevski.90@gmail.com>
2025-02-06 15:56:27 +0300

    Fixed extra

--- src/test/java/ru/aston/kovalevski_vv/task1Tests/task1Test.java
+++ src/test/java/ru/aston/kovalevski_vv/task1Tests/task1Test.java
@@ -1,7 +1,6 @@
 package ru.aston.kovalevski_vv.task1Tests;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.aston.kovalevski_vv.task1.TicketsList;
 import ru.aston.kovalevski_vv.task1.model.Commuter;
